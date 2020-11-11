72bf440d31e30afdcb6fb359d1bfd620f4fdebb9
Irrmm <grazashar@mail.ru>
2020-11-11 21:48:46 +0200

    Impl iou 2d

--- object_detection_metrics/core.py
+++ object_detection_metrics/core.py
@@ -1,2 +1,44 @@
-def iou_2d(box_a, box_b):
-    return 0
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass()
+class Box2D:
+    left: float
+    top: float
+    right: float
+    bottom: float
+
+
+def box2d_from_xyxy(box_values: List[float]) -> Box2D:
+    return Box2D(
+        left=box_values[0],
+        top=box_values[1],
+        right=box_values[2],
+        bottom=box_values[3],
+    )
+
+
+def box2d_from_xywh(box_values: List[float]) -> Box2D:
+    return Box2D(
+        left=box_values[0],
+        top=box_values[1],
+        right=box_values[0] + box_values[2],
+        bottom=box_values[1] + box_values[3],
+    )
+
+
+def iou_2d(box_a: Box2D, box_b: Box2D) -> float:
+    x_a = max(box_a.left, box_b.left)
+    y_a = max(box_a.top, box_b.top)
+    x_b = min(box_a.right, box_b.right)
+    y_b = min(box_a.bottom, box_b.bottom)
+
+    intersection = max(0.0, (x_b - x_a)) * max(0.0, (y_b - y_a))
+
+    area_a = (box_a.right - box_a.left) * (box_a.bottom - box_a.top)
+    area_b = (box_b.right - box_b.left) * (box_b.bottom - box_b.top)
+
+    union = area_a + area_b - intersection
+
+    return intersection / union
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,40 +1,37 @@
-from object_detection_metrics.core import iou_2d
+from object_detection_metrics.core import iou_2d, Box2D
 import unittest
 
 
 class IoU2DTest(unittest.TestCase):
     def test_box_a_top_left_relative_box_b_case_1(self):
         # Arrange
-        box_a = [0, 0, 4, 4]
-        box_b = [2, 2, 6, 6]
+        box_a = Box2D(0, 0, 4, 4)
+        box_b = Box2D(2, 2, 6, 6)
 
         # Act
         result = iou_2d(box_a, box_b)
 
-        print(result)
-        print(2 * 2/(4 * 4 + 4 * 4 - 2 * 2))
-
         # Assert
-        self.assertAlmostEquals(result, 2 * 2/(4 * 4 + 4 * 4 - 2 * 2), places=8)
+        self.assertAlmostEqual(result, 2 * 2 / (4 * 4 + 4 * 4 - 2 * 2), places=8)
 
     def test_box_a_top_left_relative_box_b_case_2(self):
         # Arrange
-        box_a = [20, 20, 80, 80]
-        box_b = [40, 40, 90, 90]
+        box_a = Box2D(20, 20, 80, 80)
+        box_b = Box2D(40, 40, 90, 90)
 
         # Act
         result = iou_2d(box_a, box_b)
 
         # Assert
-        self.assertAlmostEquals(result, 40 * 40/(60 * 60 + 50 * 50 - 40 * 40), places=8)
+        self.assertAlmostEqual(result, 40 * 40 / (60 * 60 + 50 * 50 - 40 * 40), places=8)
 
     def test_box_a_top_left_relative_box_b_case_3(self):
         # Arrange
-        box_a = [3, 7, 8, 9]
-        box_b = [3, 7, 8, 10]
+        box_a = Box2D(3, 7, 8, 9)
+        box_b = Box2D(3, 7, 8, 10)
 
         # Act
         result = iou_2d(box_a, box_b)
 
         # Assert
-        self.assertAlmostEquals(result, 5 * 2/(5 * 2 + 5 * 3 - 5 * 2), places=8)
+        self.assertAlmostEqual(result, 5 * 2 / (5 * 2 + 5 * 3 - 5 * 2), places=8)
