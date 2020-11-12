a52b281aea747ffdbff93b46639c3112ca2ce684
Irrmm <grazashar@mail.ru>
2020-11-12 22:02:33 +0200

    Add top right cases iou tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -35,3 +35,36 @@ class IoU2DTest(unittest.TestCase):
 
         # Assert
         self.assertAlmostEqual(result, 5 * 2 / (5 * 2 + 5 * 3 - 5 * 2), places=8)
+
+    def test_box_a_top_right_relative_box_b_case_1(self):
+        # Arrange
+        box_a = Box2D(400, 30.5, 420, 50)
+        box_b = Box2D(350, 35.7, 410, 52.01)
+
+        # Act
+        result = iou_2d(box_a, box_b)
+
+        # Assert
+        self.assertAlmostEqual(result, 10 * 14.3 / (20 * 19.5 + 60 * 16.31 - 10 * 14.3), places=8)
+
+    def test_box_a_top_right_relative_box_b_case_2(self):
+        # Arrange
+        box_a = Box2D(20, 12, 36, 13.5)
+        box_b = Box2D(20, 12.5, 36, 14)
+
+        # Act
+        result = iou_2d(box_a, box_b)
+
+        # Assert
+        self.assertAlmostEqual(result, 16 * 1 / (16 * 1.5 + 16 * 1.5 - 16 * 1), places=8)
+
+    def test_box_a_top_right_relative_box_b_case_3(self):
+        # Arrange
+        box_a = Box2D(10, 10, 20, 20)
+        box_b = Box2D(5, 20, 20, 30)
+
+        # Act
+        result = iou_2d(box_a, box_b)
+
+        # Assert
+        self.assertAlmostEqual(result, 0, places=8)
