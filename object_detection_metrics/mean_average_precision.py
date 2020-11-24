9a21079e85c39e1ac77afd25a76108c87546ecd5
Irrmm <grazashar@mail.ru>
2020-11-24 17:44:55 +0200

    Add mAP mock methods

--- object_detection_metrics/mean_average_precision.py
+++ object_detection_metrics/mean_average_precision.py
@@ -1,2 +1,21 @@
-def mAP():
+import numpy as np
+from object_detection_metrics.core import Average
+from typing import Tuple
+
+
+def mAP_2d(ground_truth: np.ndarray,
+           predicted: np.ndarray,
+           confidences: np.ndarray,
+           thresholds: Tuple = tuple(0.5),
+           average=Average.MACRO
+           ) -> float:
+    pass
+
+
+def mAP_3d(ground_truth: np.ndarray,
+           predicted: np.ndarray,
+           confidences: np.ndarray,
+           thresholds: Tuple = tuple(0.5),
+           average=Average.MACRO
+           ) -> float:
     pass
