a9e88db506640a4d59c5a646a24dbc808b44ca52
Irrmm <grazashar@mail.ru>
2020-11-25 13:06:00 +0200

    Replace methods on class for calculation mean average precision

--- object_detection_metrics/mean_average_precision.py
+++ object_detection_metrics/mean_average_precision.py
@@ -1,21 +1,31 @@
-import numpy as np
-from object_detection_metrics.core import Average
-from typing import Tuple
+from enum import Enum
+from typing import Tuple, Dict
 
 
-def mAP_2d(ground_truth: np.ndarray,
-           predicted: np.ndarray,
-           confidences: np.ndarray,
-           thresholds: Tuple = tuple(0.5),
-           average=Average.MACRO
-           ) -> float:
-    pass
+class BoxMatchingPolicy(Enum):
+    GREEDY = 'greedy'
+    SOFT = 'soft'
 
 
-def mAP_3d(ground_truth: np.ndarray,
-           predicted: np.ndarray,
-           confidences: np.ndarray,
-           thresholds: Tuple = tuple(0.5),
-           average=Average.MACRO
-           ) -> float:
-    pass
+class MAP2DCalculator:
+    def __init__(self,
+                 class_count: int,
+                 iou_thresholds: Tuple = tuple(0.5),
+                 policy=BoxMatchingPolicy.GREEDY
+                 ):
+        self.class_count = class_count
+        self.iou_thresholds = iou_thresholds
+        self.policy = policy
+
+    def calculate_mAP(self,
+                      ground_truth: Dict,
+                      predicted: Dict
+                      ) -> float:
+        """
+        Calculate mAP score
+
+        :param ground_truth: {image_id : numpy array of truth boxes: [x, y, x, y, id, difficult, crowd]}
+        :param predicted: {image_id : numpy array of truth boxes: [x, y, x, y, id, confidence]}
+        :return: mAP score in [0.0 - 1.0]
+        """
+        pass
