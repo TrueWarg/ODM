5792aa3d6d55fc453a7bc9af4982d938e879d017
Irrmm <grazashar@mail.ru>
2020-11-25 17:01:07 +0200

    Change calculate_mAP method

--- object_detection_metrics/mean_average_precision.py
+++ object_detection_metrics/mean_average_precision.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Collection
 
 
 class BoxMatchingPolicy(Enum):
@@ -9,23 +9,26 @@ class BoxMatchingPolicy(Enum):
 
 class MAP2DCalculator:
     def __init__(self,
-                 class_count: int,
+                 class_ids: Tuple,
                  iou_thresholds: Tuple = tuple(0.5),
                  policy=BoxMatchingPolicy.GREEDY
                  ):
-        self.class_count = class_count
+        self.class_ids = class_ids
         self.iou_thresholds = iou_thresholds
         self.policy = policy
 
     def calculate_mAP(self,
-                      ground_truth: Dict,
-                      predicted: Dict
+                      ground_truth: Collection,
+                      predicted: Collection
                       ) -> float:
         """
-        Calculate mAP score
+        Calculate mAP score. Params ground_truth and predicted must be have same length.
 
-        :param ground_truth: {image_id : numpy array of truth boxes: [x, y, x, y, id, difficult, crowd]}
-        :param predicted: {image_id : numpy array of truth boxes: [x, y, x, y, id, confidence]}
+        :param ground_truth: images per numpy arrays of truth boxes: [x, y, x, y, id, difficult, crowd]}
+        :param predicted: images per numpy arrays of predicted boxes: [x, y, x, y, id, confidence]}
         :return: mAP score in [0.0 - 1.0]
         """
-        pass
+        images_count = len(ground_truth)
+        for _ in range(images_count):
+            for class_id in self.class_ids:
+                pass
